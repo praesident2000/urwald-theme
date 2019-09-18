c2fd494197b7fd9ba2ceb2acfe60e1b4a517893e
Adrian <ab@buerodespraesidenten.de>
2019-09-18 09:28:23 +0200

    Start working on basic templates.

--- index.php
+++ index.php
@@ -15,9 +15,8 @@
 
 $context = Timber::get_context();
 $context['posts'] = new Timber\PostQuery();
-$context['foo'] = 'bar';
 $templates = array( 'index.twig' );
 if ( is_home() ) {
-	array_unshift( $templates, 'front-page.twig', 'home.twig' );
+	array_unshift( $templates, 'home.twig' );
 }
 Timber::render( $templates, $context );
