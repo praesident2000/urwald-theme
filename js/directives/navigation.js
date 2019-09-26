0230c681363015125b9c0909d7768f646576113f
Adrian <ab@buerodespraesidenten.de>
2019-09-26 11:16:57 +0200

    Add simple navigation script. WiP

--- js/directives/navigation.js
+++ js/directives/navigation.js
@@ -1,17 +1,31 @@
 const $ = require('jquery');
 
+const $window  = $(window);
 const header  = $('.header');
 const item = $('.menu-item-has-children .nav-main__link');
+
+let position = $window.scrollTop(); 
 
 item.on('click', (event) => {
     event.preventDefault();
     header.toggleClass('header--active');
 });
 
-
- window.addEventListener('click', (event) => {
+window.addEventListener('click', (event) => {
     const $target = $(event.target)
     if ( !$target.parents('.header').length > 0 ) {
-        $(header).removeClass('header--active');
+        header.removeClass('header--active');
     }
+});
+
+$window.scroll(() => {
+    let scroll = $window.scrollTop();
+    if( scroll > position ) {
+        header.removeClass('header--show');
+        console.log('scrollDown');
+    } else {
+        header.addClass('header--show');
+        console.log('scrollUp');
+    }
+    position = scroll;
 });
