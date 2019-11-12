134efc47623d2cb80cf96df8f677759c86e01000
Adrian <ab@buerodespraesidenten.de>
2019-11-12 12:23:11 +0100

    Add article archive page template.

---
+++ archive-article.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template Name: Wald & Wissen (Artikel)
+ */
+
+$templates = array( 'archive-article.twig' );
+
+$context = Timber::get_context();
+$context['posts'] = new Timber\PostQuery();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+
+$args_articles = array(
+	'post_type' => 'urwald_article',
+	'orderby' => 'date',
+	'order' => 'DESC',
+	'posts_per_page' => 10,
+	'paged' => $paged
+);
+
+$categories = get_categories();
+
+$context['categories'] = $categories;
+$context['articles'] = new Timber\PostQuery( $args_articles );
+
+Timber::render( $templates, $context );
