d518b83a014ba60fa6aea06ce1ee0c8fcc4d529f
Adrian <ab@buerodespraesidenten.de>
2019-11-13 19:16:38 +0100

    Rework of main navigation.

--- js/directives/navigation.js
+++ js/directives/navigation.js
@@ -4,21 +4,22 @@ const $window  = $(window);
 const header  = $('.header');
 const item = $('.menu-item-has-children .nav-main__link');
 
-let position = $window.scrollTop(); 
+let position = $window.scrollTop();
+let open; 
 
 item.on('click', (event) => {
     event.preventDefault();
-    const $parent = $(event.target).parent();
-    $parent.toggleClass('active');
+    const name = $(event.target).parent().attr('id');
+    $('.' + name ).toggleClass('active');
     header.toggleClass('header--active');
 });
 
-window.addEventListener('click', (event) => {
-    const $target = $(event.target)
-    if ( !$target.parents('.header').length > 0 ) {
-        header.removeClass('header--active');
-    }
+$('.header .hamburger').on('click', function () {
+    $(this).toggleClass('hamburger--active');
+    $('.header').toggleClass('header--active');
+    $('.nav-item-hamburger').toggleClass('active');
 });
+
 
 window.addEventListener('scroll', () => {
     let scroll = $window.scrollTop();
@@ -27,22 +28,25 @@ window.addEventListener('scroll', () => {
         if( scroll > position ) {
             header.removeClass('header--show');
             header.removeClass('header--active');
-            console.log('scrollDown');
+            // console.log('scrollDown');
         }
         else {
             header.addClass('header--show');
             header.addClass('header--active');
-            console.log('scrollUp');
+            // console.log('scrollUp');
         }
         position = scroll;
     } else {
         header.addClass('header--show');
         header.removeClass('header--active');
-        console.log('scrollTop');
+        // console.log('scrollTop');
     }
 });
 
-$('.header .hamburger').on('click', function () {
-    $(this).toggleClass('hamburger--active');
-    $('.header').toggleClass('header--active');
+// remove header active if clicked outside
+window.addEventListener('click', (event) => {
+    const $target = $(event.target)
+    if ( !$target.parents('.header').length > 0 ) {
+        header.removeClass('header--active');
+    }
 });
