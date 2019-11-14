bafef04d6869554917d6b04851d29b17f38d080f
Adrian <ab@buerodespraesidenten.de>
2019-11-14 18:27:32 +0100

    Add auto play to header slider.

--- js/directives/header-slider.js
+++ js/directives/header-slider.js
@@ -1,6 +1,7 @@
 const Flickity = require('flickity');
 
 const flkty = new Flickity( '.header-slider', {
+    autoPlay: 5000,
     cellAlign: 'left',
     cellSelector: ".header-slider__slide",
     pageDots: false,
