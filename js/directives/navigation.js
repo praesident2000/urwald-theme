09c9d036882866341933897c971bbee41cdc4ab6
Adrian <ab@buerodespraesidenten.de>
2019-11-14 15:38:09 +0100

    Rework navigation bevaviour and header template.

--- js/directives/navigation.js
+++ js/directives/navigation.js
@@ -1,26 +1,44 @@
-const $ = require('jquery');
+const $        = require('jquery');
 
 const $window  = $(window);
-const header  = $('.header');
-const item = $('.menu-item-has-children .nav-main__link');
+const header   = $('.header');
+const navs     = $('.menu-item-has-children .nav-main__link');
+const items    = $('.header__submenu');
 
-let position = $window.scrollTop();
-let open; 
+let position   = $window.scrollTop();
+let open       = false; 
 
-item.on('click', (event) => {
+// handle click events for submenu parents
+navs.on('click', (event) => {
     event.preventDefault();
-    const name = $(event.target).parent().attr('id');
-    $('.' + name ).toggleClass('active');
-    header.toggleClass('header--active');
+    event.stopPropagation();
+    const $parent = $(event.target).closest('.menu-item-has-children');
+    const $parentId = $parent.attr('id');
+
+    if (open == false && !$parent.hasClass('active') ) {
+        open = true;
+        $parent.addClass('active');
+        items.removeClass('active');
+        $('.' + $parentId ).addClass('active');
+        header.addClass('header--active');
+    } 
+    else if ( open == true && !$parent.hasClass('active') ) {
+        open = true;
+        navs.closest('.menu-item-has-children').removeClass('active');
+        $parent.addClass('active');
+        items.removeClass('active');
+        $('.' + $parentId ).addClass('active');
+    }
+    else if ( open == true && $parent.hasClass('active') ) {
+        open = true;
+        $parent.removeClass('active');
+        items.removeClass('active');
+        // $('.' + $parentId ).removeClass('active');
+        // header.removeClass('header--active');
+    }
 });
 
-$('.header .hamburger').on('click', function () {
-    $(this).toggleClass('hamburger--active');
-    $('.header').toggleClass('header--active');
-    $('.nav-item-hamburger').toggleClass('active');
-});
-
-
+// handle header scroll behaviour
 window.addEventListener('scroll', () => {
     let scroll = $window.scrollTop();
     console.log(scroll);
@@ -28,25 +46,31 @@ window.addEventListener('scroll', () => {
         if( scroll > position ) {
             header.removeClass('header--show');
             header.removeClass('header--active');
-            // console.log('scrollDown');
+            items.removeClass('active');
+            navs.closest('.menu-item-has-children').removeClass('active');
         }
         else {
             header.addClass('header--show');
             header.addClass('header--active');
-            // console.log('scrollUp');
         }
         position = scroll;
-    } else {
+    } 
+    else if ( scroll == 0 && open == true ) {
+
+    }
+    else {
         header.addClass('header--show');
-        header.removeClass('header--active');
-        // console.log('scrollTop');
+        header.removeClass('header--active'); 
     }
 });
 
-// remove header active if clicked outside
+// remove all active classes if clicked outside
 window.addEventListener('click', (event) => {
     const $target = $(event.target)
     if ( !$target.parents('.header').length > 0 ) {
+        open = false;
         header.removeClass('header--active');
+        items.removeClass('active');
+        navs.closest('.menu-item-has-children').removeClass('active');
     }
 });
