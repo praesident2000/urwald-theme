b9c15b739dbee6372d071d5754b707861e6cc128
Adrian <ab@buerodespraesidenten.de>
2019-11-28 10:02:46 +0100

    Add filtering to post archive masonry.

--- js/directives/masonry.js
+++ js/directives/masonry.js
@@ -1,5 +1,11 @@
+const $       = require('jquery');
 const Shuffle = require('shufflejs');
 
 const shuffleInstance = new Shuffle(document.getElementById('post-masonry'), {
     itemSelector: '.tease',
+});
+
+$("select.post-filter__select").change(function(){
+    var selectedCategory = $(this).children("option:selected").val();
+    shuffleInstance.filter(selectedCategory);
 });
