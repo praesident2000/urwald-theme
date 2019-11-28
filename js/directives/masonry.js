c5125ea44a0d10f166a177c5d25f47d365c2aa81
Adrian <ab@buerodespraesidenten.de>
2019-11-28 11:49:46 +0100

    Small update to mansonry filter (WiP)

--- js/directives/masonry.js
+++ js/directives/masonry.js
@@ -9,17 +9,20 @@ const shuffleInstance = new Shuffle(document.getElementById('post-masonry'), {
     itemSelector: '.tease',
 });
 
-$("select.post-filter__select").change(function(){
-    var selectedCategory = $(this).children("option:selected").val();
-    shuffleInstance.filter(selectedCategory);
-});
-
 var infScroll = new InfiniteScroll( '.post-masonry', {
     path: '.pagination__next',
     button: '.pagination__next',
     scrollThreshold: false,
     append: '.tease',
     history: false,
+});
+
+// TO DO: Update filter function to load all posts into the shuffle instance and then filter
+$("select.post-filter__select").change(function(){
+    var selectedCategory = $(this).children("option:selected").val();
+    infScroll.loadNextPage(); // WiP Load all posts instead
+    observer.observe();
+    shuffleInstance.filter(selectedCategory);
 });
 
 infScroll.on( 'append', function( response, path, items ) {
