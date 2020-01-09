ae3cf4186370ccef470f4ebc03fdc6e04810e566
Adrian <ab@buerodespraesidenten.de>
2020-01-09 12:20:36 +0100

    Add highlight element to contact page.

--- contact.php
+++ contact.php
@@ -7,6 +7,7 @@ $context = Timber::get_context();
 
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
+$context['is_contact'] = true;
 $templates = array( 'contact.twig' );
 
 Timber::render( $templates , $context );
