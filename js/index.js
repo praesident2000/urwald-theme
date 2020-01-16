a733e302b9004df8df8d83f76a9fc16158f22cd9
Adrian <ab@buerodespraesidenten.de>
2020-01-16 11:53:48 +0100

    Update outdoor active functions on discover page.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,5 @@
 const bootstrap     = require('bootstrap');
 const lozad         = require('./directives/lozad.js');
 const nav           = require('./directives/navigation.js');
-// const cookieconsent = require('./directives/cookieconsent.js');
 const share         = require('./directives/share-button.js');
 const read_more     = require('./directives/read-more.js');
