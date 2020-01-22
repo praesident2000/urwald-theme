399a0816c4ad19ed819be61149cb71af5301f63f
Adrian <ab@buerodespraesidenten.de>
2020-01-22 11:51:00 +0100

    Fix: Use correct fade function. Use OA https call.

--- js/directives/navigation.js
+++ js/directives/navigation.js
@@ -92,5 +92,5 @@ $(document).ready(function(){
 });
 
 $('#newsletter-close').on('click', () => {
-    $(this).closest('.newsletter__result').fade();
+    $(this).closest('.newsletter__result').fadeOut();
 });
