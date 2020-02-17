e086e65ad95b769eefa336d14209500189dbd59e
Adrian <ab@buerodespraesidenten.de>
2020-02-17 13:36:57 +0100

    Update hype map. Fix read more button. Update theme.

--- js/directives/read-more.js
+++ js/directives/read-more.js
@@ -1,5 +1,5 @@
 const $ = require('jquery');
 
 $('.read-more__button').on('click', (event)=> {
-    $(event.target).parent().toggleClass('read-more--open');
+    $(event.target).closest('.read-more').toggleClass('read-more--open');
 });
